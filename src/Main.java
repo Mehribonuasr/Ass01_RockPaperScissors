e265ae08e8ac905ffd188bf2c62f7038dc533376
Mehribonu <asroromu@mail.edu>
2025-10-06 22:09:23 -0400

    1. Created new project 2. Added Scanner input 3. Added variables 4. Added boolean 5. Added do and if statements 6. Added input and output statements 7. Ran code and tested 8. Completed task

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,60 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
+        String playerA = "";
+        String playerB = "";
+        boolean done = false;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        do {
+            System.out.println("Enter a move for A");
+            playerA = in.nextLine();
+            // Check for three conditions R, P, or S
+            if (playerA.equalsIgnoreCase("R") || playerA.equalsIgnoreCase("P") || playerA.equalsIgnoreCase("S")) {
+
+            }
+
+            System.out.println("Enter a move for B");
+            playerB = in.nextLine();
+            // check for three conditions R, P, or S
+            if (playerB.equalsIgnoreCase("P") || playerB.equalsIgnoreCase("S") || playerB.equalsIgnoreCase("R")) {
+
+            }
+            if (playerA.equalsIgnoreCase(playerB)) {
+                System.out.println("It's a tie");
+
+            }else if (playerA.equalsIgnoreCase("R") && playerB.equalsIgnoreCase("S")) {
+                System.out.println("Rock breaks Scissors! Player A wins!");
+
+            }else if (playerA.equalsIgnoreCase("P") && playerB.equalsIgnoreCase("R")) {
+                System.out.println("Paper covers Rock! Player A wins!");
+
+            }else if (playerA.equalsIgnoreCase("S") && playerB.equalsIgnoreCase("P")) {
+                System.out.println("Scissor cuts Paper! Player A wins!");
+
+            }else if (playerB.equalsIgnoreCase("R") && playerA.equalsIgnoreCase("S")) {
+                System.out.println("Rock breaks Scissors! Player B wins!");
+
+            }else if (playerB.equalsIgnoreCase("P") && playerA.equalsIgnoreCase("R")) {
+                System.out.println("Paper covers Rock! Player B wins!");
+
+            }else if (playerB.equalsIgnoreCase("S") && playerA.equalsIgnoreCase("P")) {
+                System.out.println("Scissors cuts Paper! Player B wins!");
+
+            }
+            else
+                System.out.println("Your input is incorrect, try again.");
+
+                System.out.println("Play again? (Y/N)");
+                String again = in.nextLine();
+
+                if (!again.equalsIgnoreCase("Y")) {
+                    done = true;
+                }
+
+            }
+            while (!done) ;
+
     }
 }
+
